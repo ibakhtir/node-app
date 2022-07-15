beda82ea05167ae9637954fc4f31871894836129
ibakhtir <ibakhtir@gmail.com>
2022-07-15 16:14:25 +0300

    added remove command

--- notes.controller.js
+++ notes.controller.js
@@ -1,40 +1,53 @@
-const fs = require('fs/promises')
-const path = require('path')
-const chalk = require('chalk')
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
 
-const notesPath = path.join(__dirname, 'db.json')
+const notesPath = path.join(__dirname, "db.json");
 
 async function addNote(title) {
-  const notes = await getNotes()
+  const notes = await getNotes();
   const note = {
     title,
-    id: Date.now().toString()
-  }
+    id: Date.now().toString(),
+  };
 
-  notes.push(note)
+  notes.push(note);
 
-  await saveNotes(notes)
-  console.log(chalk.bgGreen('Note was added!'))
+  await saveNotes(notes);
+  console.log(chalk.bgGreen("Note was added!"));
 }
 
+// Remove
+
+async function removeNote(id) {
+  const notes = await getNotes();
+  const newNotes = notes.filter((obj) => obj.id !== id);
+  await saveNotes(newNotes);
+  console.log(chalk.bgRed("Note was removed"));
+}
+
+// --end
+
 async function getNotes() {
-  const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
-  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
 }
 
 async function saveNotes(notes) {
-  await fs.writeFile(notesPath, JSON.stringify(notes))
+  await fs.writeFile(notesPath, JSON.stringify(notes));
 }
 
 async function printNotes() {
-  const notes = await getNotes()
+  const notes = await getNotes();
 
-  console.log(chalk.bgBlue('Here is the list of notes:'))
-  notes.forEach(note => {
-    console.log(chalk.bgWhite(note.id), chalk.blue(note.title))
-  })
+  console.log(chalk.bgBlue("Here is the list of notes:"));
+  notes.forEach((note) => {
+    console.log(chalk.blue(note.id), chalk.blue(note.title));
+  });
 }
 
 module.exports = {
-  addNote, printNotes
-}
+  addNote,
+  removeNote,
+  printNotes,
+};
