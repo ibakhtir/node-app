97bf29e87964ebc6e18981e48f5b441cf9a546ff
ibakhtir <ibakhtir@gmail.com>
2022-07-15 20:58:27 +0300

    added edit method

--- index.js
+++ index.js
@@ -1,45 +1,66 @@
-const express = require('express')
-const chalk = require('chalk')
-const path = require('path')
-const {addNote, getNotes, removeNote} = require('./notes.controller')
+const express = require("express");
+const chalk = require("chalk");
+const path = require("path");
+const {
+  addNote,
+  getNotes,
+  editNote,
+  removeNote,
+} = require("./notes.controller");
 
-const port = 3000
-const app = express()
+const port = 3000;
+const app = express();
 
-app.set('view engine', 'ejs')
-app.set('views', 'pages')
+app.set("view engine", "ejs");
+app.set("views", "pages");
 
-app.use(express.static(path.resolve(__dirname, 'public')))
-app.use(express.urlencoded({
-  extended: true
-}))
-
-app.get('/', async (req, res) => {
-  res.render('index', {
-    title: 'Express App',
-    notes: await getNotes(),
-    created: false
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use(
+  express.urlencoded({
+    extended: true,
   })
-})
+);
+app.use(express.json());
 
-app.post('/', async (req, res) => {
-  await addNote(req.body.title)
-  res.render('index', {
-    title: 'Express App',
+app.get("/", async (req, res) => {
+  res.render("index", {
+    title: "Express App",
     notes: await getNotes(),
-    created: true
-  })
-})
+    created: false,
+  });
+});
 
-app.delete('/:id', async (req, res) => {
-  await removeNote(req.params.id)
-  res.render('index', {
-    title: 'Express App',
+app.post("/", async (req, res) => {
+  await addNote(req.body.title);
+  res.render("index", {
+    title: "Express App",
     notes: await getNotes(),
-    created: false
-  })
-})
+    created: true,
+  });
+});
+
+// put
+
+app.put("/:id", async (req, res) => {
+  await editNote(req.params.id, req.body);
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: false,
+  });
+});
+
+// --end
+
+app.delete("/:id", async (req, res) => {
+  await removeNote(req.params.id);
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: false,
+  });
+});
 
 app.listen(port, () => {
-  console.log(chalk.green(`Server has been started on port ${port}...`))
-})
+  console.log(chalk.green(`Server has been started on port ${port}...`));
+});
--- public/app.js
+++ public/app.js
@@ -1,14 +1,40 @@
-document.addEventListener('click', event => {
-  if (event.target.dataset.type === 'remove') {
-    const id = event.target.dataset.id
+document.addEventListener("click", (event) => {
+  if (event.target.dataset.type === "remove") {
+    const id = event.target.dataset.id;
 
     remove(id).then(() => {
-      event.target.closest('li').remove()
-    })
+      event.target.closest("li").remove();
+    });
   }
-})
 
+  // edit
+
+  if (event.target.dataset.type === "edit") {
+    const id = event.target.dataset.id;
+    const newTitle = prompt("Введите новое название");
+
+    if (newTitle) {
+      const putMethod = {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ data: newTitle }),
+      };
+
+      edit(id, putMethod).then(() => {
+        const actionBlock = document.querySelector(".action-block");
+        const el = document.querySelector(`[data-id="${id}"]`);
+        el.closest("li").innerHTML = `${newTitle} ${actionBlock.outerHTML}`;
+      });
+    }
+  }
+});
 
 async function remove(id) {
-  await fetch(`/${id}`, {method: 'DELETE'})
+  await fetch(`/${id}`, { method: "DELETE" });
+}
+
+async function edit(id, method) {
+  await fetch(`/${id}`, method);
 }
